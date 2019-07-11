da34cb44865bcca862d782b7ed6390a4215db4d6
zzw <kenzzw@163.com>
2019-07-11 23:46:22 +0800

    finish practice06 and test pass

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class " + getKlass() + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return klass!=0?super.introduce()+" I am a Teacher. I teach Class " + getKlass() + ".":super.introduce()+" I am a Teacher. I teach No Class.";
+    }
 }

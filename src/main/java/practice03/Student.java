7f75bb805a9fabcd261aa0cfabe29ad077647747
zzw <kenzzw@163.com>
2019-07-11 23:26:28 +0800

    finish practice03 and test pass

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,22 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class " + getKlass() + ".";
+    }
 }
